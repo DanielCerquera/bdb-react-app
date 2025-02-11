6276c9dfd213dfb79991f8d7acdc1a354ce007c4
dcerquebbg <dcerque@bancodebogota.com.co>
2025-02-11 17:27:48 -0500

    Cambio from 1

--- src/components/molecules/ProductForm.js
+++ src/components/molecules/ProductForm.js
@@ -31,7 +31,7 @@ const ProductForm = ({ onSubmit }) => {
 
     return (
         <div className="product-form">
-            <Input type="text" name="name" placeholder="Nombre producto 5" value={product.name} onChange={handleChange} />
+            <Input type="text" name="name" placeholder="Nombre cambio" value={product.name} onChange={handleChange} />
             <Input type="text" name="description" placeholder="Descripción 5" value={product.description} onChange={handleChange} />
             <Input type="number" name="price" placeholder="Precio" value={product.price} onChange={handleChange} />
 
