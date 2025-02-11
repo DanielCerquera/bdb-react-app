8fdbe6d496e92e5df483a58b0466ca689afdf81d
dcerquebbg <dcerque@bancodebogota.com.co>
2025-02-11 17:33:35 -0500

    Inicializar gh-pages

---
+++ src/components/atoms/Button/Button.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Button.scss";
+
+const Button = ({ label, onClick, type = "primary" }) => {
+  return <button className={`button button--${type}`} onClick={onClick}>{label}</button>;
+};
+
+export default Button;
+
+// Cambio de información
---
+++ src/components/atoms/Textarea/Textarea.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Textarea.scss";
+
+const Textarea = (props) => {
+  return <textarea className="textarea" {...props} />;
+};
+
+export default Textarea;
